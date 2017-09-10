62118f138b160b6a4242c0858548c5f40c106e9a
Sean <swbrooks87@gmail.com>
2017-09-10 09:46:12 -0400

    Completed modal page

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,11 @@
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+import Modal from "./modules/Modal";
 import $ from "jquery";
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".portfolio-item"), "85%");
 new RevealOnScroll($(".bio"), "60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
